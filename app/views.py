4ce28547596fa53f369c3a103141fe242d6f50d8
stolstule <notaz4321@Gmail.com>
2023-08-25 20:58:02 +0300

    feggeg

--- app/views.py
+++ app/views.py
@@ -2,7 +2,12 @@ from django.shortcuts import render
 
 # Create your views here.
 
+<<<<<<< HEAD
 
 def fgeggr(self):
     v = 'fffgrgrff'
     x = 'grgrg'
+=======
+def fixfix(self):
+    x = 'fefefgrgrg'
+>>>>>>> maini
